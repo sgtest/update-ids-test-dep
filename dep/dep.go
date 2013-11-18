855015c1cc131c662e08ba0365587e29ced092be
Quinn Slack <qslack@qslack.com>
2013-11-18 03:10:17 -0800

    otherbase ref

--- dep/dep.go
+++ dep/dep.go
@@ -2,8 +2,10 @@ package dep
 
 import (
 	"github.com/sgtest/update-ids-test-base/base"
+	"github.com/sgtest/update-ids-test-otherbase/otherbase"
 )
 
 func Bar() {
 	base.Foo()
+	otherbase.Qux()
 }
